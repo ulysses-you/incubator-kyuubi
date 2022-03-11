83b03fabce729ffb2f5b82d93bc00e4d5e8a8aa9
ulysses-you <ulyssesyou18@gmail.com>
2022-03-11 16:51:19 +0800

    6

--- externals/kyuubi-spark-sql-engine/src/test/scala/org/apache/kyuubi/engine/spark/IndividualSparkSuite.scala
+++ externals/kyuubi-spark-sql-engine/src/test/scala/org/apache/kyuubi/engine/spark/IndividualSparkSuite.scala
@@ -85,6 +85,11 @@ class SparkEngineSuites extends KyuubiFunSuite {
   test("test engine submit timeout") {
     val timeout = 180000
     val submitTime = System.currentTimeMillis() - timeout
+    // scalastyle:off
+    println(System.getProperty(s"spark.$KYUUBI_ENGINE_SUBMIT_TIME_KEY"))
+    println(System.getProperty(s"spark.${ENGINE_INIT_TIMEOUT.key}"))
+    println("===============================")
+    // scalastyle:on
     withSystemProperty(Map(
       s"spark.$KYUUBI_ENGINE_SUBMIT_TIME_KEY" -> String.valueOf(submitTime),
       s"spark.${ENGINE_INIT_TIMEOUT.key}" -> String.valueOf(timeout))) {
@@ -96,6 +101,10 @@ class SparkEngineSuites extends KyuubiFunSuite {
 //      assert(SparkSQLEngine.currentEngine.isEmpty)
 //      assert(e1.startsWith("The total engine initialization time"))
     }
+    // scalastyle:off
+    println(System.getProperty(s"spark.$KYUUBI_ENGINE_SUBMIT_TIME_KEY"))
+    println(System.getProperty(s"spark.${ENGINE_INIT_TIMEOUT.key}"))
+    // scalastyle:on
   }
 
   ignore("test engine create sparksession timeout") {
